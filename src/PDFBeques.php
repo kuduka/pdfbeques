7494e83c527044c5181d39f1f4f3c223bfa160e2
kuduk <marc@fite.cat>
2020-05-14 14:38:47 +0200

    Apply fixes from StyleCI (#1)

--- src/PDFBeques.php
+++ src/PDFBeques.php
@@ -2,73 +2,72 @@
 
 namespace Beques;
 
-require_once('./vendor/autoload.php');
+require_once './vendor/autoload.php';
 
 use mikehaertl\pdftk\Pdf;
-use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 class PDFBeques
 {
+    public $file;
+    public $data;
 
-    var $file;
-    var $data;
-
-    function __construct($file) 
+    public function __construct($file)
     {
-	    $this->file = $file;
-	    $this->flatten();
-
+        $this->file = $file;
+        $this->flatten();
     }
 
-    function flatten()
+    public function flatten()
     {
-	$pdf = new Pdf($this->file);
-        $this->data = array_reduce($pdf->getDataFields()->getArrayCopy(), function($acc, $field) {
+        $pdf = new Pdf($this->file);
+        $this->data = array_reduce($pdf->getDataFields()->getArrayCopy(), function ($acc, $field) {
             $acc[$field['FieldName']] = $field;
+
             return $acc;
         }, []);
     }
 
-    function getFieldValue($field)
+    public function getFieldValue($field)
     {
-        return $this->data[$field]["FieldValue"];
+        return $this->data[$field]['FieldValue'];
     }
 
-
-    function getHeaders()
+    public function getHeaders()
     {
-        $headers = array();
-        foreach($this->data as $row ) {
-            array_push($headers, $row["FieldName"]);
+        $headers = [];
+        foreach ($this->data as $row) {
+            array_push($headers, $row['FieldName']);
         }
+
         return $headers;
     }
 
-    function writeExcel($file) {
-	//headers
-    $heading = $this->getHeaders();
-	$rowNumberH = 1;
-	$colH = 'A';
-	$spreadsheet = new \PhpOffice\PhpSpreadsheet\Spreadsheet();
-	$sheet = $spreadsheet->getActiveSheet();
-	foreach($heading as $h){
-		$spreadsheet->setActiveSheetIndex(0)->setCellValue($colH.$rowNumberH, $h);
-		$spreadsheet->setActiveSheetIndex(0)->getStyle($colH.$rowNumberH)->getFont()->setBold(true);
-		$colH++;
-	}
-	$spreadsheet->setActiveSheetIndex(0)->setAutoFilter('A1:E1'); //TODO: dynamic
-	//content
-    
-    $rowNumberH = 2;
-	$colH = 'A';
+    public function writeExcel($file)
+    {
+        //headers
+        $heading = $this->getHeaders();
+        $rowNumberH = 1;
+        $colH = 'A';
+        $spreadsheet = new \PhpOffice\PhpSpreadsheet\Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet();
+        foreach ($heading as $h) {
+            $spreadsheet->setActiveSheetIndex(0)->setCellValue($colH.$rowNumberH, $h);
+            $spreadsheet->setActiveSheetIndex(0)->getStyle($colH.$rowNumberH)->getFont()->setBold(true);
+            $colH++;
+        }
+        $spreadsheet->setActiveSheetIndex(0)->setAutoFilter('A1:E1'); //TODO: dynamic
+        //content
 
-    foreach($this->data as $row ) {
-        $spreadsheet->setActiveSheetIndex(0)->setCellValue($colH.$rowNumberH, $row["FieldValue"]);
-		$colH++;
-	}
-    //write document
-	$writer = new Xlsx($spreadsheet);
+        $rowNumberH = 2;
+        $colH = 'A';
+
+        foreach ($this->data as $row) {
+            $spreadsheet->setActiveSheetIndex(0)->setCellValue($colH.$rowNumberH, $row['FieldValue']);
+            $colH++;
+        }
+        //write document
+        $writer = new Xlsx($spreadsheet);
         $writer->save($file);
     }
 }
